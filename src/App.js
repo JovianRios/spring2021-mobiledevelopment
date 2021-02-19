39c09b27037ee556438777ebb9a3ae82e3209d41
JovianRios <36941795+JovianRios@users.noreply.github.com>
2021-02-19 15:40:19 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Example from './components/example/index'
+
+function App() {
+  return (
+    <div><Example/></div>
+  );
+}
+export default App;
---
+++ src/components/example/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Example extends React.Component { 
+    render()
+    {
+        return <p>Hi, my name is Jovian</p>
+    }
+}
+export default Example;
